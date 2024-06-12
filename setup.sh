7c90f448d5fcf3cbf6f74a06ee6f518aca9dfb7d
terrnitllc <terrnitllc.work@gmail.com>
2024-06-12 13:54:03 +0500

    added gitconfig setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ if ! [ -x "$(command -v ansible)"]; then
 	pip install ansible
 fi
 
+
 # Setup ssh keys
 SSH_DIR="$HOME/.ssh"
 
@@ -18,12 +19,43 @@ if ! [[ -f "$SSH_DIR/id_rsa" ]]; then
 	chmod 600 "$SSH_DIR/authorized_keys"
 fi
 
-# Run ansible
+# Check if dotfiles directory exists
 DOTFILES_DIR="$HOME/.dotfiles"
 
 if ! [[ -f "$DOTFILES_DIR" ]]; then
 	mkdir -p "$DOTFILES_DIR"
 fi
+
+# Setup gitconfig
+if ! [[ -f "$HOME/.gitconfig" ]]; then
+	# echo "[GITCONFIG]: Your global git config will be saved to ~/.gitconfig"
+
+	GIT_NAME="$1"
+	GIT_EMAIL="$2"
+
+	if [ -z "$1" ]; then
+		echo "[GITCONFIG]: Please provide github name"
+		exit 1
+	fi
+
+	if [ -z "$2" ]; then
+		echo "[GITCONFIG]: Please provide github email"
+		exit 1
+	fi
+
+	echo $1
+	echo $2
+
+	if [[ -f "$DOTFILES_DIR/templates/gitconfig" ]]; then
+		
+		cd "$DOTFILES_DIR"
+		cat "templates/gitconfig" | sed -e "s/NAME/$GIT_NAME/g" -e "s/EMAIL/$GIT_EMAIL/g" > "$HOME/.gitconfig"
+
+		cat "$HOME/.gitconfig"
+	fi
+fi
+
+# 
 
 if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
 	cd "$DOTFILES_DIR"
