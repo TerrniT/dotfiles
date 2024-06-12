7f8381f5ea62118ee0b3189e189e0b0f8a99a3e0
terrnitllc <terrnitllc.work@gmail.com>
2024-06-12 15:31:20 +0500

    ansible docker/tmux/tmp setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+# Setup python and pip
+if ! [[ -x "$(command -v pip)" ]]; then
+	sudo python3 -m ensurepip || sudo apt install pip
+	echo "[dotfiles]: pip installed"
+fi
+
 # Install ansible
 if ! [ -x "$(command -v ansible)"]; then
-	pip install ansible
+	pip install ansible --quiet
+	echo "[dotfiles]: ansible installed"
 fi
 
 
@@ -34,12 +41,12 @@ if ! [[ -f "$HOME/.gitconfig" ]]; then
 	GIT_EMAIL="$2"
 
 	if [ -z "$1" ]; then
-		echo "[GITCONFIG]: Please provide github name"
+		echo "[dotfiles]: Please provide github name for gitconfig"
 		exit 1
 	fi
 
 	if [ -z "$2" ]; then
-		echo "[GITCONFIG]: Please provide github email"
+		echo "[dotfiles]: Please provide github email for gitconfig"
 		exit 1
 	fi
 
@@ -55,12 +62,16 @@ if ! [[ -f "$HOME/.gitconfig" ]]; then
 	fi
 fi
 
-# 
 
-if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
+
+# Run Ansible
+if [[ -f "$DOTFILES_DIR/ansible/requirements.yml" ]]; then
 	cd "$DOTFILES_DIR"
 
-	ansible-galaxy install -r requirements.yml
+	ansible-galaxy install -r ansible/requirements.yml
+	echo "[dotfiles]: Installed Ansible requirements"
 fi
 
-ansible-playbook --diff "$DOTFILES_DIR/main.yml"
+
+ansible-playbook -i ansible/hosts ansible/main.yml --ask-become-pass
+echo "[dotfiles]: Running Ansible playbook"
